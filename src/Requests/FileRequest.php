b5e46815649c047da51c7f1678712e5dd122da0c
Ryan Yannelli <ryanyannelli@gmail.com>
2026-03-13 13:21:51 -0400

    refactor(requests): update namespaces to remove "Wrapper" suffix
    Signed-off-by: Ryan Yannelli <ryanyannelli@gmail.com>

--- src/Requests/FileRequest.php
+++ src/Requests/FileRequest.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Nextvisit\ClaimMDWrapper\Requests;
+namespace Nextvisit\ClaimMD\Requests;
 
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Utils;
 use InvalidArgumentException;
-use Nextvisit\ClaimMDWrapper\Client;
+use Nextvisit\ClaimMD\Client;
 use Psr\Http\Message\StreamInterface;
 
 /**
